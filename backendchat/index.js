75fc5c99e5eef8f2b81e3993f9fdc03b90a2888a
MoatafaRabie <mostaga.kxkk34@gmail.com>
2026-02-08 18:23:14 +0200

    Update index.js

--- backendchat/index.js
+++ backendchat/index.js
@@ -40,12 +40,13 @@ app.get("/api/user/currentchatters", isLogin, getcurrentchatters);
 const startServer = async () => {
     try {
         await connectoDP();
-        server.listen(3001, () => {
+        server.listen(8000, () => {
             console.log("Server is running on port 3001 and Socket.io is ready!");
         });
     } catch (error) {
         console.log("Database connection failed", error);
     }
 }
+
 
 startServer();
