1640e7257335dafdd9efa4151a70ba10079b88c9
Mostafa Moahmed Rabie Eid Abdelhamid <mostaga.kxkk34@gmail.com>
2026-05-14 13:41:54 +0300

    Update socket.js

--- backendchat/Socket/socket.js
+++ backendchat/Socket/socket.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
   cors: {
     origin: ["https://frontendchat1.vercel.app"],
     methods: ["GET", "POST"],
-    credentials: true,
+    credentials: false,
     allowedHeaders: ["Content-Type", "Authorization"]
   },
   transports: ["polling", "websocket"],
